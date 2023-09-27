8fb88f314c1506a4bfd08b1222430ef42e32739d
qeithva <38012100+qeithva@users.noreply.github.com>
2023-09-28 02:57:46 +0400

    w1

--- custom-message-on-user-lock/src/main/java/com/github/jawwy/keycloak/custommessageuserlock/CustomUsernameFormAuthenticator.java
+++ custom-message-on-user-lock/src/main/java/com/github/jawwy/keycloak/custommessageuserlock/CustomUsernameFormAuthenticator.java
@@ -1,11 +1,7 @@
 package com.github.jawwy.keycloak.custommessageuserlock;
 
-import lombok.extern.jbosslog.JBossLog;
 import org.keycloak.authentication.authenticators.browser.UsernamePasswordForm;
-import org.keycloak.credential.PasswordCredentialProvider;
-import org.keycloak.models.UserModel;
 
-@JBossLog
 public class CustomUsernameFormAuthenticator extends UsernamePasswordForm {
 
     @Override

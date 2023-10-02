b3220b6bd31fa6cf67f72008dd622a85e057a241
qeithva <38012100+qeithva@users.noreply.github.com>
2023-10-02 14:57:53 +0400

    w1

--- update-password-event-listener-extension/src/main/java/com/github/jawwy/keycloak/updatepassword/UpdatePasswordEventListener.java
+++ update-password-event-listener-extension/src/main/java/com/github/jawwy/keycloak/updatepassword/UpdatePasswordEventListener.java
@@ -87,7 +87,7 @@ public class UpdatePasswordEventListener implements EventListenerProvider {
             EmailTemplateProvider emailProvider = session.getProvider(EmailTemplateProvider.class);
             emailProvider.setRealm(realm);
             emailProvider.setUser(user);
-            // Don't forget to add the welcome-email.ftl (html and text) template to your theme.
+            // Don't forget to add the event-update_password.ftl (html and text) template to your theme.
             emailProvider.send("updatePasswordEmailSubject", subjectParams, "event-update_password.ftl", mailBodyAttributes);
         } catch (EmailException eex) {
             log.errorf(eex, "Failed to send UpdatePassword confirmation email. realm=%s user=%s", realm.getId(), user.getUsername());
